0b794f1520e64694c5a3b5a48f4de504811794c5
Keane Nguyen <kn2465@columbia.edu>
2021-06-14 23:52:41 -0400

    update logger

--- logger.py
+++ logger.py
@@ -13,14 +13,18 @@ class Logger(object):
 
     def __init__(self, log_dir):
         """Create a summary writer logging to log_dir."""
-        self.writer = tf.summary.FileWriter(log_dir)
+        #self.writer = tf.summary.FileWriter(log_dir)
+        self.writer = tf.summary.create_file_writer(log_dir)
 
     def scalar_summary(self, tag, value, step):
         """Log a scalar variable."""
-        summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
+        with self.writer.as_default():
+            tf.summary.scalar(tag, value, step)
+            self.writer.flush()
+        #summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
         # summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value.tolist())])  # .tolist() is a wierd way to convert GPU tensor to float
-        self.writer.add_summary(summary, step)
-        self.writer.flush()
+        #self.writer.add_summary(summary, step)
+        #self.writer.flush()
 
     def image_summary(self, tag, images, step):
         """Log a list of images."""
