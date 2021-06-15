8412c476539bf15f825e583871f01d1876769fc7
Keane Nguyen <kn2465@columbia.edu>
2021-06-14 23:53:25 -0400

    Sync up on config

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 CROP_SIZE = 512
 DATA_DIR = '../../data/original'
-TRAIN_DIR = './train'
-TEST_DIR = './test'
+TRAIN_DIR = f'{DATA_DIR}/stage_2_train_images'
+TEST_DIR = f'{DATA_DIR}/stage_2_test_images'
 CACHE_DIR = './cache'
